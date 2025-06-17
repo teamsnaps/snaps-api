e216006d401eeb10ad1c908139af52cbb5939507
ask4git <ask4git@gmail.com>
2025-06-17 15:01:02 +0900

    :construction_worker: feat: modified migrations

--- snapsapi/config/settings/base.py
+++ snapsapi/config/settings/base.py
@@ -306,6 +306,6 @@ SOCIALACCOUNT_ADAPTER = 'snapsapi.apps.users.adapters.CustomSocialAccountAdapter
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 # AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_S3_STATIC_BUCKET_NAME')
-AWS_STORAGE_BUCKET_NAME = 'snapsapi-app-storage' # Todo must edit env variable
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_S3_STATIC_BUCKET_NAME', 'snapsapi-app-storage') # Todo must edit env variable
 AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
 # AWS_S3_CUSTOM_DOMAIN    = 'storage.snaps.show'       # CloudFront 커스텀 도메인
